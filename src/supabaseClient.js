13732c6b1185e2ac2f06b1d0cb96adaf8d60204e
Tomoka Ninomiya <Tomoka.t1203@gmail.com>
2026-01-20 19:12:13 +0900

    supabase 修正

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://agnhgorpujrowcgsccqm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnbmhnb3JwdWpyb3djZ3NjY3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NTYyMzYsImV4cCI6MjA3NDUzMjIzNn0.FqjiOtVV5wMdF3O1iTF86Y9b0GU3pgk4qR3RI1iV3VM';
+const supabaseUrl = 'https://jbhqplcnlwxxxrqfbgmp.supabase.co';
+const supabaseAnonKey = 'sb_publishable_zSmucfz8OlKHaqjK0P71hA_11ywvysY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
